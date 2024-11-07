eeead61ba6094b8ecb3714fc8c2403d03b7d2892
Artem Naumov <naumovartem272@gmail.com>
2024-11-08 03:15:20 +0400

    Add constructor to Request

--- framework/Http/Request.php
+++ framework/Http/Request.php
@@ -6,8 +6,23 @@ namespace Framework\Http;
 
 final readonly class Request
 {
+    public function __construct(
+        public array $getParams,
+        public array $postParams,
+        public array $cookies,
+        public array $files,
+        public array $server,
+    ) {
+    }
+
     public static function createFromGlobals(): static
     {
-        return new static($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+        return new static(
+            getParams: $_GET,
+            postParams: $_POST,
+            cookies: $_COOKIE,
+            files: $_FILES,
+            server: $_SERVER,
+        );
     }
 }
